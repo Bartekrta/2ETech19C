64e598de842a519b4f5fb8208e054d63b9169023
21-BW <101757848+Bartekrta@users.noreply.github.com>
2022-12-14 11:06:26 +0100

    Create KartaObiektowa2.cs

---
+++ KartaObiektowa2.cs
@@ -0,0 +1,92 @@
+using System.Runtime;
+using System.Transactions;
+
+class Produkt
+{
+    string nazwa;
+    double cena;
+    string opis;
+
+    public string getNazwa()
+    {
+        return nazwa;
+    }
+    public double getCena()
+    {
+        return cena;
+    }
+    public string getOpis()
+    {
+        return opis;
+    }
+    public Produkt(string nazwa, double cena, string opis)
+    {
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.opis = opis;
+    }
+}
+class Koszyk
+{
+    int id;
+    Produkt produkt;
+    Uzytkownik uzytkownik;
+
+    public int getId()
+    {
+        return id;
+    }
+    public void operation()
+    {
+        Console.WriteLine("Operation");
+    }
+    public Koszyk(int id,Produkt produkt)
+    {
+        this.id = id;
+        this.produkt = produkt;
+    }
+    public void setUzytkownik(Uzytkownik uzytkownik)
+    {
+        this.uzytkownik = uzytkownik;
+    }
+}
+class Uzytkownik
+{
+    string login;
+    string haslo;
+    Profil profil;
+
+    public void loguj()
+    {
+        Console.WriteLine("Zalogowano");
+    }
+    public Uzytkownik(Profil profil, string login, string haslo)
+    {
+        this.profil = profil;
+        this.login = login;
+        this.haslo = haslo;
+    }
+}
+class Profil
+{
+    string imie;
+    string drugieimie;
+    string nazwisko;
+
+    public string getImie()
+    {
+        return imie;
+    }
+    public string getNazwisko()
+    {
+        return nazwisko;
+    }
+}
+
+class Program
+{
+    public void Main(string[] args)
+    {
+
+    }
+}
